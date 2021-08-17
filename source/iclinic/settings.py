8bf1c2bcedbdb2550255998c3aa4befc57ac1b3a
robsonlopes <robson.lopes@edu.unifor.br>
2021-08-17 00:17:03 -0300

    ajuste do settings

--- source/iclinic/settings.py
+++ source/iclinic/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'postgres',
         'PORT': 5432,
     }
 }
